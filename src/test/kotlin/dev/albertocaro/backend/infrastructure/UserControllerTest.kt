d01fc5b1c1e002ef003a24e96793ffeae674e7d0
Alberto Caro <albertcaronava@gmail.com>
2025-03-07 19:18:15 -0600

    Prueba update

--- src/test/kotlin/dev/albertocaro/backend/infrastructure/UserControllerTest.kt
+++ src/test/kotlin/dev/albertocaro/backend/infrastructure/UserControllerTest.kt
@@ -1,6 +1,8 @@
 package dev.albertocaro.backend.infrastructure
 
 import dev.albertocaro.backend.JwtInterceptor
+import dev.albertocaro.backend.data.UserRepository
+import dev.albertocaro.backend.infrastructure.user.dto.UserDeserializationDto
 import dev.albertocaro.backend.infrastructure.user.dto.UserSerializationDto
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -9,7 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForEntity
 import kotlin.test.assertEquals
@@ -49,4 +55,42 @@ class UserControllerTest @Autowired constructor(
         assertNotNull(response.body)
         assertEquals(1, response.body!!.size)
     }
+
+    @Test
+    @Transactional
+    fun `should can create a new user`() {
+        val newUser = UserDeserializationDto("alberto", "hola123", "me@albertocaro.dev", "Alberto", "Caro")
+        val headers = HttpHeaders().apply {
+            contentType = MediaType.APPLICATION_JSON
+        }
+        val request = HttpEntity(newUser, headers)
+
+        val response =
+            restTemplate.postForEntity("$baseUrl/api/users", request, UserSerializationDto::class.java)
+
+        assertEquals(HttpStatus.OK, response.statusCode)
+        assertNotNull(response.body)
+    }
+
+    @Test
+    @Transactional
+    fun `should can edit an existent user`() {
+        val newUser = UserDeserializationDto("alberto.caro", "picapapas", "alberto.caro", "Alberto", "Caro Navarro")
+        val headers = HttpHeaders().apply {
+            contentType = MediaType.APPLICATION_JSON
+        }
+        val request = HttpEntity(newUser, headers)
+
+        val response =
+            restTemplate.postForEntity("$baseUrl/api/users", request, UserSerializationDto::class.java)
+
+        assertEquals(HttpStatus.OK, response.statusCode)
+        assertNotNull(response.body)
+        assertEquals(response.body!!.name, newUser.name)
+        assertEquals(response.body!!.username, newUser.username)
+        assertEquals(response.body!!.email, newUser.email)
+        assertEquals(response.body!!.lastName, newUser.lastName)
+    }
+
+
 }
